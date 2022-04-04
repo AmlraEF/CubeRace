2bc86f43eee430e377c4e0070f30959bb3d1953d
AmiraEF <amira2296@gmail.com>
2022-04-04 13:19:28 +0100

    adding unity project files

---
+++ Assets/Scripts/EndTrigger.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndTrigger : MonoBehaviour
+{
+    public GameManager gameManager;
+    public bool triggered = false;
+
+    void OnTriggerEnter ()
+    {
+        triggered = true;
+        gameManager.CompleteLevel();
+    }
+}
---
+++ Assets/Scripts/Level.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Level : MonoBehaviour
+{
+    public Text levelText;
+       void Update()
+    {
+        levelText.text = "Level 0" + SceneManager.GetActiveScene().buildIndex;
+    }
+}
